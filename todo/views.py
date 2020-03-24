ce55660599d8fe35b468ea8f3f654130bf3c5206
Rob <reenosmith@gmail.com>
2020-03-24 19:40:04 +1100

    Added login feature

--- todo/views.py
+++ todo/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login
 
 def signupuser(request):
     if request.method == 'GET':
@@ -8,8 +10,16 @@ def signupuser(request):
     else:
         if request.POST['password1'] == request.POST['password2']:
             # Create a new user
-            user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
-            user.save()
+            try:
+                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user.save()
+                login(request, user)
+                return redirect('currenttodos')
+            except IntegrityError:
+                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Username already exists. Please use a different username'})
         else:
             print("hello")
-            # Tell the users the password didnt match
+            return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error':'Passwords did not match'})
+
+def currenttodos(request):
+    return render(request, 'todo/currenttodos.html')
